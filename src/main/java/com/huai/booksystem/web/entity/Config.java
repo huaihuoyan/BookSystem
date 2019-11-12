92a12c7f2e0c602e6729998b8b49ab68e10004eb
laiyunjing <huaihuoyan@163.com>
2019-11-12 23:28:29 +0800

    JPA和mysql配置

---
+++ src/main/java/com/huai/booksystem/web/entity/Config.java
@@ -0,0 +1,35 @@
+package com.huai.booksystem.web.entity;
+
+import javax.persistence.*;
+
+/**
+ * 网站配置
+ * @Created by laiyunjing
+ * @Date 2019/11/12 22:02
+ * @Version
+ */
+@Entity
+@Table(name="T_CONFIG")
+public class Config {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Integer id;
+    @Column(length = 50)
+    private String webName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getWebName() {
+        return webName;
+    }
+
+    public void setWebName(String webName) {
+        this.webName = webName;
+    }
+}
