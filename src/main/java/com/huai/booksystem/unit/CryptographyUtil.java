0fdcfdb0a0252ac6961590c2f2aae46fff18313e
huai <huaihuoyan@163.com>
2019-11-15 17:21:43 +0800

    密码加密

---
+++ src/main/java/com/huai/booksystem/unit/CryptographyUtil.java
@@ -0,0 +1,31 @@
+//package com.huai.booksystem.unit;
+//
+///**
+// *
+// * @Author: laiyunjing
+// * @Date: 2019/11/15 0015 16:04
+// * @Version 1.0
+// */
+//public class CryptographyUtil {
+//
+//    public static void main(String[] args) throws Exception {
+//        //用于测试加密程序
+//        System.out.println(md5("123456", "java"));
+//
+//    }
+//
+//    /**
+//     * Md5加密  加盐
+//     *
+//     * @param str
+//     *            加密的内容
+//     * @param salt
+//     *            盐值
+//     */
+//
+//    //这个方法主要是为了在我们将密码传入数据库时进行加密使用
+//    public static String md5(String pwd, String salt) {
+//        return new Md5Hash(pwd, salt).toString();
+//    }
+//
+//}
---
+++ src/main/java/com/huai/booksystem/unit/UserDao.java
@@ -0,0 +1,19 @@
+package com.huai.booksystem.unit;
+
+import com.huai.booksystem.web.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * @Author: laiyunjing
+ * @Date: 2019/11/15 0015 16:13
+ * @Version 1.0
+ */
+public interface UserDao extends JpaRepository<User,Integer> , JpaSpecificationExecutor<User> {
+
+    @Query(value = "select * from T_USER where id = ?1",nativeQuery = true)
+    public User findId(Integer id);
+
+    public User findByName(String name);
+}
